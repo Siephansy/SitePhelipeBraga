25c2f22e10cbd754e8e170e61bbb2f8e433a5a18
Phelipe Braga <49958979+Siephansy@users.noreply.github.com>
2024-03-19 15:58:11 -0300

    newnokey

--- js/upload.js
+++ js/upload.js
@@ -1,25 +1,61 @@
-const projectId = "seu-project-id";
-const keyFilename = "caminho/para/arquivo-de-credenciais.json";
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const { Storage } = require("@google-cloud/storage");
+const Multer = require("multer");
+const src = path.join(__dirname, "views");
+app.use(express.static(src));
 
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // No larger than 5mb, change as you need
+  },
+});
+
+let projectId = "sagaproedtechltda"; // Get this from Google Cloud
+let keyFilename = "mykey.json"; // Get this from Google Cloud -> Credentials -> Service Accounts
 const storage = new Storage({
   projectId,
   keyFilename,
 });
+const bucket = storage.bucket("sagaprotest"); // Get this from Google Cloud -> Storage
 
-const bucket = storage.bucket("seu-bucket-name");
+// Gets all files in the defined bucket
+app.get("/upload", async (req, res) => {
+  try {
+    const [files] = await bucket.getFiles();
+    res.send([files]);
+    console.log("Success");
+  } catch (error) {
+    res.send("Error:" + error);
+  }
+});
+// Streams file upload to Google Storage
+app.post("/upload", multer.single("imgfile"), (req, res) => {
+  console.log("Made it /upload");
+  try {
+    if (req.file) {
+      console.log("File found, trying to upload...");
+      const blob = bucket.file(req.file.originalname);
+      const blobStream = blob.createWriteStream();
 
-const file = bucket.file("nome-do-arquivo.ext");
-
-const formData = new FormData();
-formData.append("file", file);
-
-fetch("/upload", {
-  method: "POST",
-  body: formData,
-})
-.then((response) => {
-  // Exibe feedback ao usuário sobre o status do upload
-})
-.catch((error) => {
-  // Trata erros durante o upload
+      blobStream.on("finish", () => {
+        res.status(200).send("Success");
+        console.log("Success");
+      });
+      blobStream.end(req.file.buffer);
+    } else throw "error with img";
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// Get the main index html file
+app.get("/", (req, res) => {
+  res.sendFile(src + "/upload.html");
+});
+// Start the server on port 8080 or as defined
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
